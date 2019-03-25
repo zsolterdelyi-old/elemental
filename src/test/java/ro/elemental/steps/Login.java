b2496bed008bf8a167714223d811ed2c79df954b
zsolterdelyi <erdelyizsoltcsaba@gmail.com>
2019-03-25 19:12:10 +0200

    -first

---
+++ src/test/java/ro/elemental/steps/Login.java
@@ -0,0 +1,9 @@
+package ro.elemental.steps;
+
+import cucumber.api.java.en.Given;
+
+public class Login {
+    @Given("^User start the homepage$")
+    public void userStartTheHomepage() {
+    }
+}
