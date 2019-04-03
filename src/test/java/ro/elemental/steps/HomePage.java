1f7d2b4472a8d31135cc2a7a47ec82caed23bae2
zsolterdelyi <erdelyizsoltcsaba@gmail.com>
2019-04-03 18:25:41 +0300

    -language changer test 2

--- src/test/java/ro/elemental/steps/HomePage.java
+++ src/test/java/ro/elemental/steps/HomePage.java
@@ -1,5 +1,6 @@
 package ro.elemental.steps;
 
+
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import ro.elemental.TestBase;
@@ -7,6 +8,18 @@ import ro.elemental.TestBase;
 public class HomePage extends TestBase {
     @Then("^The alert message appear$")
     public void theAlertMessageAppear() {
+
+
+    }
+
+    @When("^User change the <\"([^\"]*)\">$")
+    public void userChangeThe(String language) {
+
+
+    }
+
+    @Then("^The site <\"([^\"]*)\"> will be changed to the preferred one$")
+    public void theSiteWillBeChangedToThePreferredOne(String language) {
 
 
     }
